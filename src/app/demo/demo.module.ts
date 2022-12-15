86ec302b4128123cf9de8c55f0b849d8fc34fd98
Aude Beurive <aude.beurive@bstorm.be>
2022-12-15 16:43:52 +0100

    demo pipes de bases

--- src/app/demo/demo.module.ts
+++ src/app/demo/demo.module.ts
@@ -5,12 +5,14 @@ import { DemoRoutingModule } from './demo-routing.module';
 import { DemoComponent } from './demo.component';
 import { Demo1Component } from './demo1/demo1.component';
 import { FormsModule } from '@angular/forms';
+import { Demo2Component } from './demo2/demo2.component';
 
 
 @NgModule({
   declarations: [
     DemoComponent,
-    Demo1Component
+    Demo1Component,
+    Demo2Component,
   ],
   imports: [
     CommonModule,
---
+++ src/app/demo/demo2/demo2.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-demo2',
+  templateUrl: './demo2.component.html',
+  styleUrls: ['./demo2.component.scss']
+})
+export class Demo2Component {
+    maChaine : string = 'Je suis une chaine';
+    monNombre : number = 14.14574857965;
+    maDate : Date = new Date()
+
+}
